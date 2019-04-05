22ab5b793d0f7304df93c9ab6d682bd2514278cf
HamzaT1992 <tsi.hamza1992@gmail.com>
2019-04-05 13:56:24 +0200

    fix users.index

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -22,7 +22,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('admin.users.index', compact('users', 'roles'));
+		
+        return view('admin.users.index', compact('users'));
     }
 
     /**
